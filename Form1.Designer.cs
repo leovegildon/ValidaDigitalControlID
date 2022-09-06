3966d3dbaaadb4fc7d53739559d896080cbe0ef8
Leovegildo Ferreira de Araujo Neto <leovegildon@gmail.com>
2022-09-06 12:57:10 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ValidaDigital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.captureLog = new System.Windows.Forms.Label();
+            this.pictureFinger = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFinger)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Enabled = false;
+            this.btnCapturar.Location = new System.Drawing.Point(30, 43);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(600, 34);
+            this.btnCapturar.TabIndex = 0;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 282);
+            this.textBox1.TabIndex = 1;
+            // 
+            // captureLog
+            // 
+            this.captureLog.AutoSize = true;
+            this.captureLog.Location = new System.Drawing.Point(27, 407);
+            this.captureLog.Name = "captureLog";
+            this.captureLog.Size = new System.Drawing.Size(31, 13);
+            this.captureLog.TabIndex = 2;
+            this.captureLog.Text = "Log: ";
+            // 
+            // pictureFinger
+            // 
+            this.pictureFinger.Location = new System.Drawing.Point(399, 105);
+            this.pictureFinger.Name = "pictureFinger";
+            this.pictureFinger.Size = new System.Drawing.Size(231, 282);
+            this.pictureFinger.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureFinger.TabIndex = 3;
+            this.pictureFinger.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 446);
+            this.Controls.Add(this.pictureFinger);
+            this.Controls.Add(this.captureLog);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCapturar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFinger)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label captureLog;
+        private System.Windows.Forms.PictureBox pictureFinger;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControliD;
+
+namespace ValidaDigital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ret = CIDBio.Init();
+            if (ret == RetCode.SUCCESS)
+            {
+                captureLog.Text += "Init Successful\r\n";
+                btnCapturar.Enabled = true;
+            }
+            else if (ret < RetCode.SUCCESS)
+            {
+                captureLog.Text += "Init Error: " + CIDBio.GetErrorMessage(ret) + "\r\n";
+            }
+            else
+            {
+                captureLog.Text += "Init Warning: " + CIDBio.GetErrorMessage(ret) + "\r\n";
+                btnCapturar.Enabled = true;
+            }
+        }
+
+        string t;
+        byte[] imageBuf;
+        uint width, height;
+        int quality;
+
+        public static Bitmap ImageBufferToBitmap(byte[] imageBuf, uint width, uint height)
+        {
+            Bitmap img = new Bitmap((int)width, (int)height);
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    var color = Color.FromArgb(imageBuf[x + img.Width * y], imageBuf[x + img.Width * y], imageBuf[x + img.Width * y]);
+                    img.SetPixel(x, y, color);
+                }
+            }
+            return img;
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            CIDBio cIDBio = new CIDBio();
+            cIDBio.CaptureImageAndTemplate(out t, out imageBuf, out width, out height, out quality);
+            //MessageBox.Show(imageBuf.ToString());
+            pictureFinger.Image = ImageBufferToBitmap(imageBuf, width, height);
+
+            textBox1.Text = t;
+        }
+    }
+}
